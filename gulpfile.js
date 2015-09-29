9b37c730825a2e24b02d413e0f2a57526b9a9244
Gabriel Ferreiro <gbril9119@gmail.com>
2015-09-29 20:12:36 +0200

    Added dist folder to gitignore. Included Gulp task to copy static files to dist.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
-var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
-var ts = require("gulp-typescript");
-var less = require("gulp-less");
-var path = require("path");
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var ts = require('gulp-typescript');
+var less = require('gulp-less');
+var path = require('path');
 
-gulp.task("less", function() {
-	return gulp.src(["./less/**/*.less"])
+gulp.task('less', function() {
+	return gulp.src(['./less/**/*.less'])
 		.pipe(less({
 			paths: [ path.join(__dirname, 'less') ]
 		}))
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task("typescript", function(){
-	return gulp.src(["./ts/declarations/**/*.ts", "./ts/**/*.ts"])
+gulp.task('typescript', function(){
+	return gulp.src(['./ts/declarations/**/*.ts', './ts/**/*.ts'])
 		.pipe(sourcemaps.init())
 		.pipe(ts({
 			declarationFiles : false,
 			noExternalResolve : true
 		}))
-		.pipe(sourcemaps.write("./maps"))
-		.pipe(gulp.dest('./js'));
+		.pipe(sourcemaps.write('./dist/maps'))
+		.pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task("default", ["less", "typescript"]);
+gulp.task('html', function() {
+	return gulp.src('./index.html')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['less', 'typescript', 'html']);
